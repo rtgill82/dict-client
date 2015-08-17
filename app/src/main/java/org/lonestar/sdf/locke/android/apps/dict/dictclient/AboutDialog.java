3b2906d1cf4901535bb43b26dfc5db14fbc170e1
Robert Gill <rtgill82@gmail.com>
2015-08-17 01:40:41 -0700

    Put about dialog text in raw html resource

--- app/src/main/java/org/lonestar/sdf/locke/android/apps/dict/dictclient/AboutDialog.java
+++ app/src/main/java/org/lonestar/sdf/locke/android/apps/dict/dictclient/AboutDialog.java
@@ -4,11 +4,17 @@ import org.lonestar.sdf.locke.apps.dict.dictclient.R;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class AboutDialog extends Dialog {
+    private String html;
 
     public AboutDialog(Context context) {
         super(context);
@@ -16,9 +22,22 @@ public class AboutDialog extends Dialog {
 
     @Override
     public void onCreate(Bundle savedInstance) {
+        if (html == null) {
+            Resources resources = this.getContext().getResources();
+            InputStream stream = resources.openRawResource(R.raw.about);
+
+            try {
+                byte[] buffer = new byte[stream.available()];
+                stream.read(buffer);
+                html = new String(buffer);
+            } catch (IOException e) {
+                // FIXME: Display dialog error?
+                Log.e("AboutDialog", String.format("Unable to read file about.html: %s", e.getMessage()));
+            }
+        }
         setContentView(R.layout.dialog_about);
 
         TextView textView = (TextView) findViewById(R.id.about_text);
-        textView.setText(Html.fromHtml("<h2>DictClient</h2> Version 1.0.0<br>Copyright &copy 2013 Robert Gill<br/><br/><p>Icon by <a href='http://www.boomgraphics.se'>Robin Weatherall</a></p>"));
+        textView.setText(Html.fromHtml(html));
     }
 }
