4a0c2b255bfa6949710b1a449e6f5dcc7828cfa6
Robert Gill <rtgill82@gmail.com>
2016-08-21 22:57:13 -0700

    Call super for onCreateOptionsMenu()

--- app/src/main/java/org/lonestar/sdf/locke/android/apps/dict/dictclient/MainActivity.java
+++ app/src/main/java/org/lonestar/sdf/locke/android/apps/dict/dictclient/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends FragmentActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
