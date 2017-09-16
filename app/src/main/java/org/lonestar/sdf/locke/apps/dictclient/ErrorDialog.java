f2489241d555f34fadfc5061e65f657e7a4b4e0e
Robert Gill <rtgill82@gmail.com>
2017-09-16 12:41:49 -0700

    Generalize ErrorDialog into MessageDialog

--- app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/ErrorDialog.java
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/ErrorDialog.java
@@ -9,35 +9,15 @@
 package org.lonestar.sdf.locke.apps.dictclient;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.os.Bundle;
 
-public class ErrorDialog extends DialogFragment
+public class ErrorDialog extends MessageDialog
 {
-  private static final String MESSAGE = "message";
-
   public static void show (Activity activity, String message)
   {
-    Bundle args = new Bundle ();
-    args.putString (MESSAGE, message);
-    ErrorDialog dialog = new ErrorDialog ();
-    dialog.setArguments (args);
-    dialog.show (
-      activity.getFragmentManager (),
-      ErrorDialog.class.getSimpleName ()
+    ErrorDialog.show (
+        activity,
+        activity.getString (R.string.title_error),
+        message
     );
-  }
-
-  @Override
-  public Dialog onCreateDialog (Bundle savedInstanceState)
-  {
-    AlertDialog.Builder builder = new AlertDialog.Builder (getActivity ());
-    String message = getArguments ().getString (MESSAGE);
-    builder.setTitle (getString (R.string.title_error))
-           .setMessage (message)
-           .setPositiveButton (getString (R.string.button_ok), null);
-    return builder.create ();
   }
 }
---
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/MessageDialog.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 Robert Gill <locke@sdf.lonestar.org>
+ * All rights reserved.
+ *
+ * This file is a part of DICT Client.
+ *
+ */
+
+package org.lonestar.sdf.locke.apps.dictclient;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+public class MessageDialog extends DialogFragment
+{
+  private static final String TITLE = "title";
+  private static final String MESSAGE = "message";
+
+  public static void show (Activity activity, String title, String message)
+  {
+    Bundle args = new Bundle ();
+    args.putString (TITLE, title);
+    args.putString (MESSAGE, message);
+    MessageDialog dialog = new MessageDialog();
+    dialog.setArguments (args);
+    dialog.show (
+      activity.getFragmentManager (),
+      MessageDialog.class.getSimpleName ()
+    );
+  }
+
+  @Override
+  public Dialog onCreateDialog (Bundle savedInstanceState)
+  {
+    AlertDialog.Builder builder = new AlertDialog.Builder (getActivity ());
+    String title = getArguments ().getString (TITLE);
+    String message = getArguments ().getString (MESSAGE);
+    builder.setTitle (title)
+           .setMessage (message)
+           .setPositiveButton (getString (R.string.button_ok), null);
+    return builder.create ();
+  }
+}
