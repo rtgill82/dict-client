99cae5173b4ce6ae029b5147a0bd28612befaa66
Robert Gill <rtgill82@gmail.com>
2018-05-06 14:54:57 -0700

    Add missing Copyright notifications

--- app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/DonationFlowCallbacks.java
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/DonationFlowCallbacks.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2018 Robert Gill <locke@sdf.lonestar.org>
+ * All rights reserved.
+ *
+ * This file is a part of DICT Client.
+ *
+ */
+
 package org.lonestar.sdf.locke.apps.dictclient;
 
 import android.app.Activity;
