e224eaf08b9474fa8f4a8fee0aa9377f7910cee4
Robert Gill <rtgill82@gmail.com>
2018-05-17 20:37:04 -0700

    Remove HorizontalScrollView from ResultView

--- app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/ResultView.java
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/ResultView.java
@@ -11,27 +11,19 @@ package org.lonestar.sdf.locke.apps.dictclient;
 import android.content.Context;
 import android.support.v4.widget.NestedScrollView;
 import android.util.AttributeSet;
-import android.widget.HorizontalScrollView;
-
-import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 class ResultView extends NestedScrollView {
-    private HorizontalScrollView horizontalScrollView;
     private ResultTextView resultTextView;
     private boolean wordWrap;
 
     public ResultView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        horizontalScrollView = createHorizontalScrollView();
     }
 
     @Override
     public void onFinishInflate() {
         super.onFinishInflate();
         resultTextView = findViewById(R.id.result_text_view);
-        removeAllViews();
-        horizontalScrollView.addView(resultTextView);
-        addView(horizontalScrollView);
     }
 
     public CharSequence getText() {
@@ -43,25 +35,6 @@ class ResultView extends NestedScrollView {
     }
 
     public void setWordWrap(boolean value) {
-        if (wordWrap != value) {
-            wordWrap = value;
-            removeAllViews();
-            horizontalScrollView.removeAllViews();
-            if (wordWrap) {
-                addView(resultTextView);
-            } else {
-                horizontalScrollView.addView(resultTextView);
-                addView(horizontalScrollView);
-            }
-        }
-    }
-
-    private HorizontalScrollView createHorizontalScrollView() {
-        HorizontalScrollView horizontalScrollView =
-          new HorizontalScrollView(getContext());
-        LayoutParams params = new LayoutParams(MATCH_PARENT, MATCH_PARENT);
-        horizontalScrollView.setLayoutParams(params);
-        horizontalScrollView.setFillViewport(true);
-        return horizontalScrollView;
+        wordWrap = value;
     }
 }
