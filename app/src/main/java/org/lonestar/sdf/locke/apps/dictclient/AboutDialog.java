a63088f949850b42729ed8f36459697e99085c78
Robert Gill <rtgill82@gmail.com>
2018-05-20 16:56:16 -0700

    Remove 'Ok' button from About dialog

--- app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/AboutDialog.java
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/AboutDialog.java
@@ -61,8 +61,7 @@ public class AboutDialog extends DialogFragment {
                      public void onClick(DialogInterface dialog, int which) {
                          DonateDialog.show(getActivity());
                      }
-                 })
-               .setPositiveButton(getString(R.string.button_ok), null);
+                 });
         return builder.create();
     }
 
