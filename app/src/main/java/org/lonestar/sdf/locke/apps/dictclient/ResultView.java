49ae339b75770aceb41924135d88d5c509e212e5
Robert Gill <rtgill82@gmail.com>
2018-05-19 17:54:31 -0700

    Adjust scaling on device reorientation

--- app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/ResultView.java
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/ResultView.java
@@ -9,10 +9,15 @@
 package org.lonestar.sdf.locke.apps.dictclient;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.widget.NestedScrollView;
 import android.util.AttributeSet;
 
 class ResultView extends NestedScrollView {
+    private static final String SUPER_STATE = "SUPER_STATE";
+    private static final String WORD_WRAP = "WORD_WRAP";
+
     private ResultTextView resultTextView;
     private boolean wordWrap;
 
@@ -24,6 +29,33 @@ class ResultView extends NestedScrollView {
     public void onFinishInflate() {
         super.onFinishInflate();
         resultTextView = findViewById(R.id.result_text_view);
+    }
+
+    @Override
+    public Parcelable onSaveInstanceState() {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(SUPER_STATE, super.onSaveInstanceState());
+        bundle.putBoolean(WORD_WRAP, wordWrap);
+        return bundle;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Parcelable state) {
+        Bundle bundle;
+        if (state instanceof Bundle) {
+            bundle = (Bundle) state;
+            wordWrap = bundle.getBoolean(WORD_WRAP);
+            state = bundle.getParcelable(SUPER_STATE);
+        }
+        super.onRestoreInstanceState(state);
+    }
+
+    @Override
+    public void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        resultTextView.restoreTextSize();
+        if (!wordWrap)
+          resultTextView.scaleToFitWidth();
     }
 
     public CharSequence getText() {
