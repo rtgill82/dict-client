f6f04f258279ba56fdea00baa448018e84210605
Robert Gill <rtgill82@gmail.com>
2018-05-28 17:20:49 -0700

    Fix missing check boxes in host management

--- app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/ManageHostCursorAdapter.java
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/ManageHostCursorAdapter.java
@@ -10,37 +10,48 @@ package org.lonestar.sdf.locke.apps.dictclient;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckedTextView;
 import android.widget.CursorAdapter;
-import android.widget.TextView;
 
 import org.lonestar.sdf.locke.libs.dict.JDictClient;
 
 class ManageHostCursorAdapter extends CursorAdapter {
+    private Drawable checkMarkDrawable;
+
     public ManageHostCursorAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
+        checkMarkDrawable =
+          context.getResources()
+                 .getDrawable(R.drawable.checkbox_background);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         HostCursor hostCursor = (HostCursor) cursor;
         LayoutInflater inflater = LayoutInflater.from(context);
-        CheckedTextView textview = (CheckedTextView)
+        CheckedTextView textView = (CheckedTextView)
           inflater.inflate(R.layout.list_item_host, null);
-        textview.setText(Html.fromHtml(createItem(hostCursor)));
-        if (hostCursor.isReadonly())
-          textview.setCheckMarkDrawable(null);
-        return textview;
+        textView.setText(Html.fromHtml(createItem(hostCursor)));
+        if (hostCursor.isReadonly()) {
+            textView.setCheckMarkDrawable(null);
+        }
+        return textView;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        TextView textview = (TextView) view;
-        textview.setText(Html.fromHtml(createItem(cursor)));
+        CheckedTextView textView = (CheckedTextView) view;
+        textView.setText(Html.fromHtml(createItem(cursor)));
+        textView.setCheckMarkDrawable(checkMarkDrawable);
+        HostCursor hostCursor = (HostCursor) cursor;
+        if (hostCursor.isReadonly()) {
+            textView.setCheckMarkDrawable(null);
+        }
     }
 
     @Override
@@ -56,12 +67,13 @@ class ManageHostCursorAdapter extends CursorAdapter {
           cursor.getString(cursor.getColumnIndex("description"));
 
         String itemText = "<b>" + host;
-        if (port != JDictClient.DEFAULT_PORT)
-          itemText += ":" + port.toString();
+        if (port != JDictClient.DEFAULT_PORT) {
+            itemText += ":" + port.toString();
+        }
         itemText += "</b>";
-        if (description.length() > 0)
-          itemText += "<br><i>" + description + "</i>";
-
+        if (description.length() > 0) {
+            itemText += "<br><i>" + description + "</i>";
+        }
         return itemText;
     }
 }
