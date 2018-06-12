edb306db22ebe58224ec3bdfb7d0481ae57ce46d
Robert Gill <rtgill82@gmail.com>
2018-06-11 20:30:24 -0700

    Clean up SelectHostCursorAdapter

--- app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/SelectHostCursorAdapter.java
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/SelectHostCursorAdapter.java
@@ -18,41 +18,39 @@ import android.widget.CheckedTextView;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
-import org.lonestar.sdf.locke.libs.jdictclient.JDictClient;
-
 class SelectHostCursorAdapter extends CursorAdapter {
     public SelectHostCursorAdapter(Context context, Cursor cursor, int flags) {
         super(context, cursor, flags);
     }
 
+    @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(context);
         CheckedTextView view = (CheckedTextView)
           inflater.inflate(R.layout.list_item_host, null);
-        view.setText(Html.fromHtml(createItem(cursor)));
+        HostCursor hostCursor = (HostCursor) cursor;
+        view.setText(Html.fromHtml(buildItemText(hostCursor)));
         view.setCheckMarkDrawable(null);
         return view;
     }
 
+    @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        if (view instanceof TextView) {
-            String itemText = createItem(cursor);
-            ((TextView) view).setText(Html.fromHtml(itemText));
-        }
+        HostCursor hostCursor = (HostCursor) cursor;
+        String itemText = buildItemText(hostCursor);
+        TextView textView = (TextView) view;
+        textView.setText(Html.fromHtml(itemText));
     }
 
-    private String createItem(Cursor cursor) {
-        String host = cursor.getString(cursor.getColumnIndex("name"));
-        Integer port = cursor.getInt(cursor.getColumnIndex("port"));
+    private String buildItemText(HostCursor cursor) {
+        String host = cursor.getHostName();
         String description =
           cursor.getString(cursor.getColumnIndex("description"));
 
-        String itemText = "<b>" + host;
-        if (port != JDictClient.DEFAULT_PORT)
-          itemText += ":" + port.toString();
-        itemText += "</b>";
-        if (description.length() > 0)
-          itemText += "<br><i>" + description + "</i>";
+        String itemText = "<b>" + host + "</b>";
+        if (description.length() > 0) {
+            itemText += "<br><i>" + description + "</i>";
+        }
         return itemText;
     }
 }
