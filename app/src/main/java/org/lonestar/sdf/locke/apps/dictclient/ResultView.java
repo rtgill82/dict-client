61226e8f49b281ea4c67649c46255eb4293a03ff
Robert Gill <rtgill82@gmail.com>
2019-06-14 19:04:24 -0700

    Fix issues with scrolling and LinkMovementMethod

--- app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/ResultView.java
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/ResultView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Robert Gill <locke@sdf.lonestar.org>
+ * Copyright (C) 2017 Robert Gill <locke@sdf.lonestar.org>
  * All rights reserved.
  *
  * This file is a part of DICT Client.
@@ -9,101 +9,181 @@
 package org.lonestar.sdf.locke.apps.dictclient;
 
 import android.content.Context;
-import android.content.SharedPreferences;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.preference.PreferenceManager;
-import android.support.v4.widget.NestedScrollView;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.Selection;
+import android.text.Spannable;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
 
-class ResultView extends NestedScrollView {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import static android.util.TypedValue.COMPLEX_UNIT_PX;
+
+public class ResultView extends AppCompatTextView {
     private static final String SUPER_STATE = "SUPER_STATE";
+    private static final String TEXT_SIZE = "TEXT_SIZE";
     private static final String DISPLAY_OPTION = "DISPLAY_OPTION";
 
-    private ResultTextView mResultTextView;
-    private DisplayOption mDisplayOption;
+    private final float mOrigTextSize = getTextSize();
+    private boolean mScrolling = false;
+    private DisplayOption mDisplayOption = DisplayOption.SCROLL;
+
+    private GestureDetector mGestureDetector;
 
     public ResultView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setMovementMethod(LinkMovementMethod.getInstance());
+        initGestureDetector();
     }
 
-    @Override
-    public void onFinishInflate() {
-        super.onFinishInflate();
-        mResultTextView = findViewById(R.id.result_text_view);
-        setInitialDisplayOption();
-        updateDisplay();
+    public void setDisplayOption(DisplayOption displayOption) {
+        mDisplayOption = displayOption;
+        restoreTextSize();
+        switch (mDisplayOption) {
+            case FIT_WIDTH:
+                scaleToFitWidth();
+                /* fall through */
+            case LINE_WRAP:
+                setHorizontallyScrolling(false);
+                break;
+            case SCROLL:
+                setHorizontallyScrolling(true);
+                break;
+        }
     }
 
     @Override
     public Parcelable onSaveInstanceState() {
         Bundle bundle = new Bundle();
         bundle.putParcelable(SUPER_STATE, super.onSaveInstanceState());
-        bundle.putString(DISPLAY_OPTION, mDisplayOption.toString());
+        saveState(bundle);
         return bundle;
     }
 
     @Override
     public void onRestoreInstanceState(Parcelable state) {
-        Bundle bundle;
+        Bundle bundle = null;
         if (state instanceof Bundle) {
             bundle = (Bundle) state;
-            setDisplayOption(
-                DisplayOption.valueOf(bundle.getString(DISPLAY_OPTION))
-            );
             state = bundle.getParcelable(SUPER_STATE);
         }
+
         super.onRestoreInstanceState(state);
+        if (bundle != null)
+          restoreState(bundle);
     }
 
     @Override
-    public void invalidate() {
-        super.invalidate();
-        updateDisplay();
+    protected void onLayout(boolean changed, int left, int top,
+                            int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        if (changed)
+          setDisplayOption(mDisplayOption);
     }
 
-    @SuppressWarnings("SpellCheckingInspection")
     @Override
-    public void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        updateDisplay();
-    }
-
-    public CharSequence getText() {
-        return mResultTextView.getText();
-    }
-
-    public void setText(CharSequence text) {
-        mResultTextView.setText(text);
-        updateDisplay();
-        scrollTo(0, 0);
-    }
-
-    public void setDisplayOption(DisplayOption value) {
-        mDisplayOption = value;
-    }
-
-    private void setInitialDisplayOption() {
-        Context context = this.getContext();
-        SharedPreferences prefs = PreferenceManager
-          .getDefaultSharedPreferences(context);
-        mDisplayOption = DisplayOption.valueOf(
-            prefs.getString(
-                context.getString(R.string.pref_key_display_option),
-                context.getString(R.string.pref_display_option_fit_width)
-            ));
-    }
-
-    private void updateDisplay() {
-        mResultTextView.restoreTextSize();
-        mResultTextView.setHorizontallyScrolling(false);
-        switch (mDisplayOption) {
-            case FIT_WIDTH:
-                mResultTextView.scaleToFitWidth();
-                break;
-            case SCROLL:
-                mResultTextView.setHorizontallyScrolling(true);
-                break;
+    protected void onTextChanged(CharSequence text, int start,
+                                 int lengthBefore, int lengthAfter) {
+        if (mDisplayOption == DisplayOption.FIT_WIDTH) {
+            restoreTextSize();
+            scaleToFitWidth();
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mGestureDetector.onTouchEvent(event);
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (mScrolling == true) {
+                mScrolling = false;
+                return true;
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void restoreTextSize() {
+        setTextSize(COMPLEX_UNIT_PX, mOrigTextSize);
+    }
+
+    private void scaleToFitWidth() {
+        int measuredWidth = getMeasuredWidth();
+        if (measuredWidth <= 0)
+          return;
+
+        int availableWidth =
+          measuredWidth - getTotalPaddingLeft() - getTotalPaddingRight();
+
+        if (availableWidth <= 0)
+          return;
+
+        float textSize = findOptimalTextSize(availableWidth);
+        setTextSize(COMPLEX_UNIT_PX, textSize);
+    }
+
+    private float findOptimalTextSize(int availableWidth) {
+        String text = getText().toString();
+        BufferedReader reader = new BufferedReader(new StringReader(text));
+        String longestLine = "";
+        int longestLineLength = 0;
+
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (longestLineLength < line.length()) {
+                    longestLine = line;
+                    longestLineLength = longestLine.length();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Rect rect = new Rect();
+        TextPaint paint = new TextPaint(getPaint());
+        int size = Math.round(paint.getTextSize());
+        rect.right = Integer.MAX_VALUE;
+        while (rect.right >= availableWidth) {
+            paint.getTextBounds(longestLine, 0, longestLineLength, rect);
+            if (rect.right < availableWidth) break;
+            size -= 1;
+            paint.setTextSize((float) size);
+        }
+        return (float) size;
+    }
+
+    private void saveState(Bundle outState) {
+        outState.putFloat(TEXT_SIZE, getTextSize());
+        outState.putInt(DISPLAY_OPTION, mDisplayOption.ordinal());
+    }
+
+    private void restoreState(Bundle savedInstanceState) {
+        float textSize = savedInstanceState.getFloat(TEXT_SIZE);
+        setTextSize(COMPLEX_UNIT_PX, textSize);
+        int ordinal = savedInstanceState.getInt(DISPLAY_OPTION);
+        mDisplayOption = DisplayOption.values()[ordinal];
+    }
+
+    private void initGestureDetector() {
+        mGestureDetector = new GestureDetector(this.getContext(),
+          new SimpleOnGestureListener() {
+              @Override
+              public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                      float distanceX, float distanceY) {
+                  mScrolling = true;
+                  Spannable text = (Spannable) ResultView.this.getText();
+                  Selection.removeSelection(text);
+                  return true;
+              }
+          });
     }
 }
