ab08d64de78f8a964999b177125bb9de70e8a792
Robert Gill <rtgill82@gmail.com>
2019-06-21 23:03:23 -0700

    Add CustomListPreference class

---
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/CustomListPreference.java
@@ -0,0 +1,87 @@
+package org.lonestar.sdf.locke.apps.dictclient;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.preference.ListPreference;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckedTextView;
+
+class CustomListPreference extends ListPreference {
+    private LayoutInflater mInflater;
+    private int mItemStyle;
+    private int mPad;
+
+    private CharSequence[] mEntries;
+    private CharSequence[] mEntryValues;
+
+    public CustomListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mInflater = LayoutInflater.from(context);
+        setAlertDialogStyle(context);
+        Resources resources = context.getResources();
+        mPad = (int) resources.getDimension(R.dimen.default_margins);
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
+        mEntries = getEntries();
+        mEntryValues = getEntryValues();
+        builder.setAdapter(new Adapter(), null);
+        super.onPrepareDialogBuilder(builder);
+    }
+
+    private void setAlertDialogStyle(Context context) {
+        TypedArray a = context.obtainStyledAttributes(
+            null,
+            R.styleable.AlertDialog,
+            R.attr.alertDialogStyle,
+            0
+        );
+
+        try {
+            mItemStyle = a.getResourceId(
+                R.styleable.AlertDialog_singleChoiceItemLayout,
+                android.R.layout.select_dialog_singlechoice
+            );
+        } finally {
+            a.recycle();
+        }
+    }
+
+    protected CharSequence styleText(int position, CharSequence entry) {
+        return entry;
+    }
+
+    private class Adapter extends BaseAdapter {
+        public int getCount() {
+            if (mEntries != null)
+              return mEntries.length;
+            return 0;
+        }
+
+        public Object getItem(int position) {
+            return mEntryValues[position];
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            CheckedTextView view = (CheckedTextView) convertView;
+            if (view ==  null) {
+                view = (CheckedTextView)
+                  mInflater.inflate(mItemStyle, parent, false);
+                view.setText(styleText(position, mEntries[position]));
+                view.setPadding(mPad, mPad, mPad, mPad);
+            }
+            return view;
+        }
+    }
+}
--- app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/DefaultHostListPreference.java
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/DefaultHostListPreference.java
@@ -1,105 +1,32 @@
 package org.lonestar.sdf.locke.apps.dictclient;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.preference.ListPreference;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.CheckedTextView;
 
-class DefaultHostListPreference extends ListPreference {
-    private LayoutInflater mInflater;
-    private int mItemStyle;
-    private int mPad;
-
-    private CharSequence[] mEntries;
-    private CharSequence[] mEntryValues;
-
+class DefaultHostListPreference extends CustomListPreference {
     public DefaultHostListPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mInflater = LayoutInflater.from(context);
-        setAlertDialogStyle(context);
-        Resources resources = context.getResources();
-        mPad = (int) resources.getDimension(R.dimen.default_margins);
         bindHosts();
-    }
-
-    @Override
-    protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
-        mEntries = getEntries();
-        mEntryValues = getEntryValues();
-        builder.setAdapter(new Adapter(), null);
-        super.onPrepareDialogBuilder(builder);
-    }
-
-    private void setAlertDialogStyle(Context context) {
-        TypedArray a = context.obtainStyledAttributes(
-                null,
-                R.styleable.AlertDialog,
-                R.attr.alertDialogStyle,
-                0
-        );
-
-        try {
-            mItemStyle = a.getResourceId(
-                    R.styleable.AlertDialog_singleChoiceItemLayout,
-                    android.R.layout.select_dialog_singlechoice
-            );
-        } finally {
-            a.recycle();
-        }
     }
 
     private void bindHosts() {
         HostCursor cursor = (HostCursor)
-                DatabaseManager.find(Host.class, "hidden", false);
+          DatabaseManager.find(Host.class, "hidden", false);
         CharSequence[] entries = new CharSequence[cursor.getCount()];
         CharSequence[] entryValues = new CharSequence[cursor.getCount()];
 
-        int i = 0;
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            entries[i] = cursor.getHostName();
-            entryValues[i] = Integer.toString(cursor.getId());
-            i = i + 1;
-            cursor.moveToNext();
+        try {
+            for (int i = 0; !cursor.isAfterLast(); cursor.moveToNext(), i++) {
+                entries[i] = cursor.getHostName();
+                entryValues[i] = Integer.toString(cursor.getId());
+            }
+        } finally {
+            cursor.close();
         }
 
         Host defaultHost = DictClient.getDefaultHost();
         setEntries(entries);
         setEntryValues(entryValues);
         setValue(defaultHost.getId().toString());
-    }
-
-    private class Adapter extends BaseAdapter {
-        public int getCount() {
-            if (mEntries != null)
-                return mEntries.length;
-            return 0;
-        }
-
-        public Object getItem(int position) {
-            return mEntryValues[position];
-        }
-
-        public long getItemId(int position) {
-            return position;
-        }
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-            CheckedTextView view = (CheckedTextView) convertView;
-            if (view ==  null) {
-                view = (CheckedTextView)
-                        mInflater.inflate(mItemStyle, parent, false);
-                view.setText(mEntries[position]);
-                view.setPadding(mPad, mPad, mPad, mPad);
-            }
-            return view;
-        }
     }
 }
