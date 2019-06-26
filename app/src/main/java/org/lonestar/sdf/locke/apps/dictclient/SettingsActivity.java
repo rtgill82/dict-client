4fc25849d58fcc4833d1a5ccf96f8b3db45254ca
Robert Gill <rtgill82@gmail.com>
2019-06-25 20:54:37 -0700

    SettingsActivity: fix initial Default Host summary
    Fix the initial Default Host summary on older versions of Android.
    Use ListPreference.getEntry() if entry index is not found.

--- app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/SettingsActivity.java
+++ app/src/main/java/org/lonestar/sdf/locke/apps/dictclient/SettingsActivity.java
@@ -37,15 +37,14 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 if (preference instanceof ListPreference) {
                     // For list preferences, look up the correct display value
                     // in the preference's 'entries' list.
-                    ListPreference listPreference = (ListPreference)
-                      preference;
-                    int index = listPreference.findIndexOfValue(stringValue);
+                    ListPreference listPref = (ListPreference) preference;
+                    int index = listPref.findIndexOfValue(stringValue);
 
                     // Set the summary to reflect the new value.
                     preference.setSummary(
                         index >= 0
-                            ? listPreference.getEntries()[index]
-                            : null);
+                            ? listPref.getEntries()[index]
+                            : listPref.getEntry());
                 } else if (preference.getKey()
                     .equals(context.getString(R.string.pref_key_font_size))) {
                     //  Display sp as the unit for font_size preference.
